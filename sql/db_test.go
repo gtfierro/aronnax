8927a4b1c5c5c2d5a6be25809d848c36f5befbce
Gabe Fierro <gtfierro225@gmail.com>
2015-12-03 10:04:16 -0800

    fix test format -- this is much better

--- sql/db_test.go
+++ sql/db_test.go
@@ -241,83 +241,87 @@ func TestWhereRecentDocument(t *testing.T) {
 	uuid5, _ := uuid.FromString("411ce89c-8cbd-11e5-8bb3-0cc47a0f7eea")
 
 	for _, test := range []struct {
-		querystring string             // query
-		uuids       map[uuid.UUID]bool // expected matching UUIDs are keys. Initialize vals to false
+		querystring string // query
+		uuids       []uuid.UUID
 	}{
 		{
 			"select distinct uuid where Location/Room = '410';",
-			map[uuid.UUID]bool{uuid2: false, uuid4: false},
+			[]uuid.UUID{uuid2, uuid4},
 		},
 		{
 			"select distinct uuid where Location/Room != '410';",
-			map[uuid.UUID]bool{uuid1: false, uuid3: false, uuid5: false},
+			[]uuid.UUID{uuid1, uuid3, uuid5},
 		},
 		{
 			"select distinct uuid where Location/Room like '4%';",
-			map[uuid.UUID]bool{uuid1: false, uuid2: false, uuid3: false, uuid4: false, uuid5: false},
+			[]uuid.UUID{uuid1, uuid2, uuid3, uuid4, uuid5},
 		},
 		{
 			"select distinct uuid where has Location/Room;",
-			map[uuid.UUID]bool{uuid1: false, uuid2: false, uuid3: false, uuid4: false, uuid5: false},
+			[]uuid.UUID{uuid1, uuid2, uuid3, uuid4, uuid5},
 		},
 		{
 			"select distinct uuid where Metadata/Exposure = 'South' and has Properties/Timezone;",
-			map[uuid.UUID]bool{uuid1: false},
+			[]uuid.UUID{uuid1},
 		},
 		{
 			"select distinct uuid where (Metadata/Exposure = 'South' and has Properties/Timezone);",
-			map[uuid.UUID]bool{uuid1: false},
+			[]uuid.UUID{uuid1},
 		},
 		{
 			"select distinct uuid where Location/Room = '405' and has Metadata/Exposure;",
-			map[uuid.UUID]bool{},
+			[]uuid.UUID{},
 		},
 		{
 			"select distinct uuid where Location/Room = '405' or Location/Room = '411';",
-			map[uuid.UUID]bool{uuid5: false, uuid1: false},
+			[]uuid.UUID{uuid5, uuid1},
 		},
 		{
 			"select distinct uuid where Location/Room = '405' or Location/Room = '411' or Location/Room = '420';",
-			map[uuid.UUID]bool{uuid5: false, uuid3: false, uuid1: false},
+			[]uuid.UUID{uuid5, uuid3, uuid1},
 		},
 		{
 			"select distinct uuid where Location/Room = '405' or (Location/Room = '411' and Metadata/Exposure='East');",
-			map[uuid.UUID]bool{uuid5: false},
+			[]uuid.UUID{uuid5},
 		},
 		{
 			"select distinct uuid where Location/Room = '405' or (Location/Room = '411' and Metadata/Exposure='South');",
-			map[uuid.UUID]bool{uuid5: false, uuid1: false},
+			[]uuid.UUID{uuid5, uuid1},
 		},
 		{
 			"select distinct uuid where (Location/Room = '411' and Metadata/Exposure='South') or Location/Room = '405';",
-			map[uuid.UUID]bool{uuid5: false, uuid1: false},
+			[]uuid.UUID{uuid5, uuid1},
 		},
 		{
 			"select distinct uuid where Location/Room = '405' or (Location/Room = '411' and (Location/City = 'Berkeley' and Metadata/Exposure='South'));",
-			map[uuid.UUID]bool{uuid5: false, uuid1: false},
+			[]uuid.UUID{uuid5, uuid1},
 		},
 		{
 			"select distinct uuid where (Location/Room = '411' and (Location/City = 'Berkeley' and Metadata/Exposure='South')) or Location/Room = '405'; ",
-			map[uuid.UUID]bool{uuid5: false, uuid1: false},
+			[]uuid.UUID{uuid5, uuid1},
 		},
 	} {
 		var (
-			docs []*Document
-			err  error
+			docs            []*Document
+			expectedMatches = make(map[uuid.UUID]bool)
+			err             error
 		)
+		for _, uid := range test.uuids {
+			expectedMatches[uid] = false
+		}
 		fmt.Println(test.querystring)
 		if docs, err = DocsFromRows(backend.Eval(backend.Parse(test.querystring))); err != nil {
 			t.Errorf("Query failed! %v", err)
 		}
 		for _, doc := range docs {
-			if _, found := test.uuids[doc.UUID]; !found {
+			if _, found := expectedMatches[doc.UUID]; !found {
 				t.Errorf("Query %v matched unexpected UUID %v", test.querystring, doc.UUID)
 			} else {
-				test.uuids[doc.UUID] = true
+				expectedMatches[doc.UUID] = true
 			}
 		}
 
-		for uuid, covered := range test.uuids {
+		for uuid, covered := range expectedMatches {
 			if !covered {
 				t.Errorf("Query %v did not match expected UUID %v", test.querystring, uuid)
 			}
@@ -338,35 +342,39 @@ func TestWhereWithNotRecentDocument(t *testing.T) {
 	uuiddummy, _ := uuid.FromString("aa45f708-8be8-11e5-86ae-5cc5d4ded1ae")
 
 	for _, test := range []struct {
-		querystring string             // query
-		uuids       map[uuid.UUID]bool // expected matching UUIDs are keys. Initialize vals to false
+		querystring string // query
+		uuids       []uuid.UUID
 	}{
 		{
 			"select distinct uuid where not Location/Room like '4%';",
-			map[uuid.UUID]bool{uuiddummy: false},
+			[]uuid.UUID{uuiddummy},
 		},
 		{
 			"select distinct uuid where Location/Room = '405' and not has Metadata/Exposure;",
-			map[uuid.UUID]bool{uuid5: false},
+			[]uuid.UUID{uuid5},
 		},
 	} {
 		var (
-			docs []*Document
-			err  error
+			docs            []*Document
+			expectedMatches = make(map[uuid.UUID]bool)
+			err             error
 		)
+		for _, uid := range test.uuids {
+			expectedMatches[uid] = false
+		}
 		fmt.Println(test.querystring)
 		if docs, err = DocsFromRows(backend.Eval(backend.Parse(test.querystring))); err != nil {
 			t.Errorf("Query failed! %v", err)
 		}
 		for _, doc := range docs {
-			if _, found := test.uuids[doc.UUID]; !found {
+			if _, found := expectedMatches[doc.UUID]; !found {
 				t.Errorf("Query %v matched unexpected UUID %v", test.querystring, doc.UUID)
 			} else {
-				test.uuids[doc.UUID] = true
+				expectedMatches[doc.UUID] = true
 			}
 		}
 
-		for uuid, covered := range test.uuids {
+		for uuid, covered := range expectedMatches {
 			if !covered {
 				t.Errorf("Query %v did not match expected UUID %v", test.querystring, uuid)
 			}
@@ -386,63 +394,159 @@ func TestWhereWithTimePredicateWithBefore(t *testing.T) {
 	uuid5, _ := uuid.FromString("411ce89c-8cbd-11e5-8bb3-0cc47a0f7eea")
 	uuiddummy, _ := uuid.FromString("aa45f708-8be8-11e5-86ae-5cc5d4ded1ae")
 	for _, test := range []struct {
-		querystring string             // query
-		uuids       map[uuid.UUID]bool // expected matching UUIDs are keys. Initialize vals to false
+		querystring string // query
+		uuids       []uuid.UUID
 	}{
 		// BEFORE
 		{
 			"select distinct uuid where Location/Room = '410' before 5;",
-			map[uuid.UUID]bool{uuid1: false, uuid2: false, uuid3: false, uuid4: false, uuid5: false},
+			[]uuid.UUID{uuid1, uuid2, uuid3, uuid4, uuid5},
 		},
 		{
 			"select distinct uuid where Location/Room = '410' before 6;",
-			map[uuid.UUID]bool{uuid1: false, uuid2: false, uuid3: false, uuid4: false, uuid5: false},
+			[]uuid.UUID{uuid1, uuid2, uuid3, uuid4, uuid5},
 		},
 		{
 			"select distinct uuid where Location/Room = '410' before 8;",
-			map[uuid.UUID]bool{uuid1: false, uuid2: false, uuid3: false, uuid4: false, uuid5: false},
+			[]uuid.UUID{uuid1, uuid2, uuid3, uuid4, uuid5},
 		},
 		{
 			"select distinct uuid where Location/Room = '410' before 9;",
-			map[uuid.UUID]bool{uuid1: false, uuid2: false, uuid3: false, uuid4: false, uuid5: false},
+			[]uuid.UUID{uuid1, uuid2, uuid3, uuid4, uuid5},
 		},
 		{
 			"select distinct uuid where Location/Room = '411' before 5;",
-			map[uuid.UUID]bool{},
+			[]uuid.UUID{},
 		},
 		{
 			"select distinct uuid where Location/Room = '411' before 6;",
-			map[uuid.UUID]bool{uuid1: false},
+			[]uuid.UUID{uuid1},
 		},
 		{
 			"select distinct uuid where not has Metadata/Exposure before 13;",
-			map[uuid.UUID]bool{uuid1: false, uuid2: false, uuid3: false, uuid4: false, uuid5: false, uuiddummy: false},
+			[]uuid.UUID{uuid1, uuid2, uuid3, uuid4, uuid5, uuiddummy},
 		},
 		{
 			"select distinct uuid where not has Metadata/Exposure before 14;",
-			map[uuid.UUID]bool{uuid2: false, uuid3: false, uuid4: false, uuid5: false, uuiddummy: false},
+			[]uuid.UUID{uuid2, uuid3, uuid4, uuid5, uuiddummy},
 		},
 	} {
 		var (
-			docs []*Document
-			err  error
+			docs            []*Document
+			expectedMatches = make(map[uuid.UUID]bool)
+			err             error
 		)
+		for _, uid := range test.uuids {
+			expectedMatches[uid] = false
+		}
 		fmt.Println(test.querystring)
 		if docs, err = DocsFromRows(backend.Eval(backend.Parse(test.querystring))); err != nil {
 			t.Errorf("Query failed! %v", err)
 		}
 		for _, doc := range docs {
-			if _, found := test.uuids[doc.UUID]; !found {
+			if _, found := expectedMatches[doc.UUID]; !found {
 				t.Errorf("Query %v matched unexpected UUID %v", test.querystring, doc.UUID)
 			} else {
-				test.uuids[doc.UUID] = true
+				expectedMatches[doc.UUID] = true
 			}
 		}
 
-		for uuid, covered := range test.uuids {
+		for uuid, covered := range expectedMatches {
 			if !covered {
 				t.Errorf("Query %v did not match expected UUID %v", test.querystring, uuid)
 			}
 		}
 	}
 }
+
+func TestWhereWithTimePredicateWithIBefore(t *testing.T) {
+	user := os.Getenv("ARONNAXTESTUSER")
+	pass := os.Getenv("ARONNAXTESTPASS")
+	dbname := os.Getenv("ARONNAXTESTDB")
+	backend := newBackend(user, pass, dbname)
+	uuid1, _ := uuid.FromString("2b365d6a-8cbd-11e5-8bb3-0cc47a0f7eea")
+	uuid2, _ := uuid.FromString("370dd17c-8cbd-11e5-8bb3-0cc47a0f7eea")
+	uuid3, _ := uuid.FromString("3a77a0e0-8cbd-11e5-8bb3-0cc47a0f7eea")
+	uuid4, _ := uuid.FromString("3da1cafc-8cbd-11e5-8bb3-0cc47a0f7eea")
+	uuid5, _ := uuid.FromString("411ce89c-8cbd-11e5-8bb3-0cc47a0f7eea")
+	uuiddummy, _ := uuid.FromString("aa45f708-8be8-11e5-86ae-5cc5d4ded1ae")
+	for _, test := range []struct {
+		querystring string // query
+		uuids       []uuid.UUID
+	}{
+		// IBEFORE
+		{
+			"select distinct uuid where Location/Room = '410' ibefore 8;",
+			[]uuid.UUID{uuid2, uuid4},
+		},
+		{
+			"select distinct uuid where Location/Room = '410' ibefore 6;",
+			[]uuid.UUID{uuid2, uuid3, uuid4, uuid5},
+		},
+		{
+			"select distinct uuid where not has Metadata/Exposure ibefore 13;",
+			[]uuid.UUID{uuid1, uuid2, uuid3, uuid4, uuid5, uuiddummy},
+		},
+	} {
+		var (
+			docs            []*Document
+			expectedMatches = make(map[uuid.UUID]bool)
+			err             error
+		)
+		for _, uid := range test.uuids {
+			expectedMatches[uid] = false
+		}
+		fmt.Println(test.querystring)
+		if docs, err = DocsFromRows(backend.Eval(backend.Parse(test.querystring))); err != nil {
+			t.Errorf("Query failed! %v", err)
+		}
+		for _, doc := range docs {
+			if _, found := expectedMatches[doc.UUID]; !found {
+				t.Errorf("Query %v matched unexpected UUID %v", test.querystring, doc.UUID)
+			} else {
+				expectedMatches[doc.UUID] = true
+			}
+		}
+
+		for uuid, covered := range expectedMatches {
+			if !covered {
+				t.Errorf("Query %v did not match expected UUID %v", test.querystring, uuid)
+			}
+		}
+	}
+}
+
+/*
+
+	// IAFTER
+	{
+		"select distinct uuid where Location/Room = '411' iafter 0;",
+		map[uuid.UUID]bool{},
+	},
+	{
+		"select distinct uuid where Location/Room = '411' iafter 5;",
+		map[uuid.UUID]bool{uuid1: false},
+	},
+	{
+		"select distinct uuid where has Metadata/Exposure iafter 4;",
+		map[uuid.UUID]bool{uuid1: false, uuid2: false, uuid3: false, uuid4: false, uuid5: false},
+	},
+	{
+		"select distinct uuid where has Metadata/Exposure iafter 13;",
+		map[uuid.UUID]bool{uuid1: false, uuid2: false, uuid3: false, uuid4: false, uuid5: false},
+	},
+	{
+		"select distinct uuid where has Metadata/Exposure iafter 17;",
+		map[uuid.UUID]bool{uuid1: false, uuid2: false, uuid3: false, uuid4: false},
+	},
+	{
+		"select distinct uuid where has Metadata/Exposure iafter 18;",
+		map[uuid.UUID]bool{uuid1: false, uuid2: false, uuid3: false, uuid4: false},
+	},
+
+	// AFTER
+	{
+		"select distinct uuid where has Metadata/Exposure after 17;",
+		map[uuid.UUID]bool{uuid1: false, uuid2: false, uuid3: false, uuid4: false},
+	},
+*/
